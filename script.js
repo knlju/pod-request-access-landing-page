40866d0819484dad10a33ed6d7fdf9977d85caa5
ljubisakn <ljubisakn@gmail.com>
2021-09-30 11:55:56 +0200

    fixed issues

--- script.js
+++ script.js
@@ -6,8 +6,7 @@
     function handleClick(e) {
         const emailInputValue = document.querySelector(".input-email").value
         const divErr = document.querySelector(".input-error")
-        if (emailInputValue.match(/\S+@\S+.\S+/)) {
-            divErr.classList.remove('show-error')
-        } else divErr.classList.add('show-error')
+        const emailRegex = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+        emailInputValue.match(emailRegex) ? divErr.classList.remove('show-error') : divErr.classList.add('show-error')
     }
 })()
